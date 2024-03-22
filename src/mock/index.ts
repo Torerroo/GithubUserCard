c3d3e731d58dbe0481b84847009fdd984d52bda7
Torerroo <Torerro1996@mail.ru>
2024-03-22 20:42:47 +0300

    finish

---
+++ src/mock/index.ts
@@ -0,0 +1,16 @@
+import { LocalGithubUser } from "types";
+
+export const defaultUser: LocalGithubUser = {
+  login: "Torerroo",
+  avatar: "https://avatars.githubusercontent.com/u/109388399?v=4",
+  name: "Михаил",
+  company: "",
+  blog: "",
+  location: "Cheboksary",
+  bio: "Frontend-developer",
+  twitter: "",
+  repos: 5,
+  followers: 0,
+  following: 0,
+  created: "2022-07-15T20:16:54Z",
+};
